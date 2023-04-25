dcccbbc4128dcd158aec1836ddad470ecb9c4f58
Aparna-vairagade-good-to <vairagadeaparna123456>
2023-04-25 21:59:40 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import React from 'react'
+import Routing from "./Components/Routing"
+import Navbar from "./Components/Navbar"
+
+const App = () => {
+  return (
+    <div>
+        <Navbar />
+        <Routing />
+    </div>
+  )
+}
+
+export default App
